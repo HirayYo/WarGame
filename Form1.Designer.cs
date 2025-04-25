4822c56915feb79ebb7137a5f191e3eae4589a6d
HirayYo <158539162+HirayYo@users.noreply.github.com>
2025-04-25 14:51:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SpaceDefender
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toggleSoundMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.toggleSoundMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.gameToolStripMenuItem.Text = "Игра";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newGameToolStripMenuItem.Text = "Новая игра";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // toggleSoundMenuItem
+            // 
+            this.toggleSoundMenuItem.Name = "toggleSoundMenuItem";
+            this.toggleSoundMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.toggleSoundMenuItem.Text = "Звук: Вкл";
+            this.toggleSoundMenuItem.Click += new System.EventHandler(this.ToggleSound);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1067, 504);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 532);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1067, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Space Defender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toggleSoundMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Windows.Forms;
+
+namespace SpaceDefender
+{
+    public partial class Form1 : Form
+    {
+        // Звуки
+        private SoundPlayer backgroundSound;
+        private SoundPlayer bonusSound;
+        private SoundPlayer hitSound;
+
+        // Изображения
+        private Image playerImage;
+        private Image bulletImage;
+        private Image enemyImage;
+        private Image explosionImage;
+
+        // Игровые объекты
+        private PlayerShip player;
+        private List<Bullet> playerBullets = new List<Bullet>();
+        private List<Enemy> enemies = new List<Enemy>();
+
+        // Состояния
+        private bool soundEnabled = true;
+        private int score = 0;
+        private int level = 1;
+        private bool leftPressed, rightPressed, upPressed, downPressed;
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadResources();
+            InitializeGame();
+
+            pictureBox1.Paint += PictureBox1_Paint;
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            this.KeyUp += Form1_KeyUp;
+        }
+
+        private void LoadResources()
+        {
+            playerImage = Image.FromFile("Resources/playerShip.png");
+            bulletImage = Image.FromFile("Resources/bullet.png");
+            enemyImage = Image.FromFile("Resources/meteor.png");
+            explosionImage = Image.FromFile("Resources/explosion.png");
+
+            backgroundSound = new SoundPlayer("Resources/backgroundMusic.wav");
+            bonusSound = new SoundPlayer("Resources/bonus.wav");
+            hitSound = new SoundPlayer("Resources/hit.wav");
+        }
+
+        private void InitializeGame()
+        {
+            if (soundEnabled)
+                backgroundSound.PlayLooping();
+
+            player = new PlayerShip(pictureBox1.Width / 2, pictureBox1.Height - 60, playerImage);
+
+            playerBullets.Clear();
+            enemies.Clear();
+            score = 0;
+            level = 1;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs eventArgs)
+        {
+            // Движение игрока
+            int speed = 5;
+            if (leftPressed && player.X > 0) player.X -= speed;
+            if (rightPressed && player.X < pictureBox1.Width) player.X += speed;
+            if (upPressed && player.Y > 0) player.Y -= speed;
+            if (downPressed && player.Y < pictureBox1.Height) player.Y += speed;
+
+            // Выстрелы
+            foreach (var bullet in playerBullets)
+                bullet.Y -= 10;
+            playerBullets.RemoveAll(bullet => bullet.Y < -10);
+
+            // Враги
+            if (rand.Next(0, 20) == 0)
+            {
+                int enemyX = rand.Next(0, pictureBox1.Width - 30);
+                enemies.Add(new Enemy(enemyX, -30, enemyImage));
+            }
+
+            foreach (var enemy in enemies)
+                enemy.Y += 3;
+            enemies.RemoveAll(enemy => enemy.Y > pictureBox1.Height);
+
+            // Коллизии
+            CheckCollisions();
+
+            pictureBox1.Invalidate();
+        }
+
+        private void CheckCollisions()
+        {
+            foreach (var bullet in playerBullets.ToList())
+            {
+                foreach (var enemy in enemies.ToList())
+                {
+                    if (Math.Abs(bullet.X - enemy.X) < 20 && Math.Abs(bullet.Y - enemy.Y) < 20)
+                    {
+                        if (soundEnabled) hitSound.Play();
+
+                        playerBullets.Remove(bullet);
+                        enemies.Remove(enemy);
+                        score += 10;
+
+                        if (score % 100 == 0)
+                        {
+                            if (soundEnabled) bonusSound.Play();
+                            level++;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void FireBullet()
+        {
+            if (player != null)
+            {
+                playerBullets.Add(new Bullet(player.X, player.Y - 20, bulletImage));
+            }
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs paintArgs)
+        {
+            Graphics g = paintArgs.Graphics;
+
+            if (player != null)
+                player.Draw(g);
+
+            foreach (var bullet in playerBullets)
+                bullet.Draw(g);
+
+            foreach (var enemy in enemies)
+                enemy.Draw(g);
+
+            g.DrawString($"Счёт: {score}   Уровень: {level}", new Font("Arial", 14, FontStyle.Bold), Brushes.White, new PointF(10, 10));
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs keyArgs)
+        {
+            if (keyArgs.KeyCode == Keys.Left || keyArgs.KeyCode == Keys.A) leftPressed = true;
+            if (keyArgs.KeyCode == Keys.Right || keyArgs.KeyCode == Keys.D) rightPressed = true;
+            if (keyArgs.KeyCode == Keys.Up || keyArgs.KeyCode == Keys.W) upPressed = true;
+            if (keyArgs.KeyCode == Keys.Down || keyArgs.KeyCode == Keys.S) downPressed = true;
+            if (keyArgs.KeyCode == Keys.Space) FireBullet();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs keyArgs)
+        {
+            if (keyArgs.KeyCode == Keys.Left || keyArgs.KeyCode == Keys.A) leftPressed = false;
+            if (keyArgs.KeyCode == Keys.Right || keyArgs.KeyCode == Keys.D) rightPressed = false;
+            if (keyArgs.KeyCode == Keys.Up || keyArgs.KeyCode == Keys.W) upPressed = false;
+            if (keyArgs.KeyCode == Keys.Down || keyArgs.KeyCode == Keys.S) downPressed = false;
+        }
+
+        private void ToggleSound(object sender, EventArgs eventArgs)
+        {
+            soundEnabled = !soundEnabled;
+            toggleSoundMenuItem.Text = soundEnabled ? "Звук: Вкл" : "Звук: Выкл";
+            if (soundEnabled)
+                backgroundSound.PlayLooping();
+            else
+                backgroundSound.Stop();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs eventArgs)
+        {
+            backgroundSound.Stop();
+            InitializeGame();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs eventArgs)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs eventArgs)
+        {
+            timer1.Interval = 33;
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+        }
+
+        // === Игровые объекты ===
+
+        public class PlayerShip
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Image ShipImage { get; set; }
+
+            public PlayerShip(int x, int y, Image image)
+            {
+                X = x;
+                Y = y;
+                ShipImage = image;
+            }
+
+            public void Draw(Graphics g)
+            {
+                g.DrawImage(ShipImage, X - 15, Y - 15, 30, 30);
+            }
+        }
+
+        public class Bullet
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Image BulletImage { get; set; }
+
+            public Bullet(int x, int y, Image image)
+            {
+                X = x;
+                Y = y;
+                BulletImage = image;
+            }
+
+            public void Draw(Graphics g)
+            {
+                g.DrawImage(BulletImage, X - 2, Y - 5, 4, 10);
+            }
+        }
+
+        public class Enemy
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Image EnemyImage { get; set; }
+
+            public Enemy(int x, int y, Image image)
+            {
+                X = x;
+                Y = y;
+                EnemyImage = image;
+            }
+
+            public void Draw(Graphics g)
+            {
+                g.DrawImage(EnemyImage, X - 15, Y - 15, 30, 30);
+            }
+        }
+    }
+}
